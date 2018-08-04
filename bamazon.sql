a2c8973dcd7b8b8c593335fc39bdf37b51cc510a
Shampu <cbuchhauser@gmail.com>
2018-08-04 00:11:34 -0700

    First commit

---
+++ bamazon.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+
+item_id INT NOT NULL AUTO_INCREMENT,
+
+product_name VARCHAR(100),
+
+department_name VARCHAR(100),
+
+price DECIMAL(10,2),
+
+quantity INT,
+
+PRIMARY KEY (item_id)
+
+);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Fish Tank", "Pets", 50.75, 10);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Chew Bone", "Pets", 14.99, 50);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Cat Toy", "Pets", 8.95, 20);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Milk", "Food", 3.99, 40);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Eggs", "Food", 6.50, 120);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Bananas", "Food", 3.50, 75);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Rug", "Furniture", 79.90, 6);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Nightstand", "Furniture", 120.00, 4);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Lamp", "Furniture", 25.00, 10);
+
+SELECT * FROM products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,8 @@
+// Run app
+// Displays all items available for sale (IDs, names, prices)
+// Select ID to purchase
+// Select how many to buy
+// Check if store has enough (If not, "Insufficient quantity")
+// If store has enough, update quantity in SQL Database
+// Show customer total cost of purchase
+
